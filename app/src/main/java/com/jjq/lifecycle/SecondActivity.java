9fe2d64f2098b8dbc740ce6e3dd9bb34a810bf71
jjq <jjq>
2016-07-22 11:12:56 +0800

    调用Activity

---
+++ app/src/main/java/com/jjq/lifecycle/SecondActivity.java
@@ -0,0 +1,9 @@
+package com.jjq.lifecycle;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by lsx on 2016/7/22.
+ */
+public class SecondActivity extends AppCompatActivity{
+}

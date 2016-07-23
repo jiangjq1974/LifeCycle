e2fe0938f16100bbd2d79429d40405bb5cdf112d
jjq <jjq>
2016-07-23 10:39:14 +0800

    Activity布局实现

--- app/src/main/java/com/jjq/lifecycle/SecondActivity.java
+++ app/src/main/java/com/jjq/lifecycle/SecondActivity.java
@@ -2,19 +2,23 @@ package com.jjq.lifecycle;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 import android.widget.Toast;
 
 /**
  * Created by lsx on 2016/7/22.
  */
 public class SecondActivity extends AppCompatActivity{
+    private TextView text;
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
         Intent intent=getIntent();
         String data=intent.getStringExtra(MainActivity.DATA);
         Toast.makeText(SecondActivity.this, data, Toast.LENGTH_SHORT).show();
+        text = (TextView) findViewById(R.id.activity_second_textview);
+        text.setText(data);
     }
 }
